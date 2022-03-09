0c1b603f6942a6613490fa89f51ef29356fa8ecf
fshlarf <faishal.arif218@gmail.com>
2022-03-09 13:08:06 +0700

    change something

---
+++ Javascript/JS 1 tipe-data/0-intro.js
@@ -0,0 +1,50 @@
+// String
+var name = "Digidev";
+var city = "Medan";
+var email = `digidev@gmail.com`;
+var emptyString = "";
+
+// Number
+var total = 1500;
+var pi = 3.14;
+var emptyNumber = 0;
+
+// Boolean
+var isOpen = true;
+var isValid = false;
+
+// Function
+var showAlert = function () {
+  alert("digidev");
+};
+var openAlert = () => {
+  alert("digidev");
+};
+function setAlert() {
+  alert("digidev");
+}
+
+// Object
+var student = {
+  name: "Faishal",
+  age: 26,
+};
+var emptyObject = {};
+
+// Array
+var fruits = ["apel", "mangga", "nenas"];
+var shoesNumbers = [37, 38, 39, 40, 41, 42];
+var students = [
+  {
+    name: "Faishal",
+    age: 26,
+  },
+  {
+    name: "Ibnu",
+    age: 26,
+  },
+];
+var emptyArray = [];
+
+// REGEX
+var regex = /\D/i;
